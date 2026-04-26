7cdf4156a3aff44b7130f4ef9c955a24c00a11b2
Emmie Gemme <mimiemmie@gmail.com>
2026-04-26 06:25:20 -0400

    Feat: Acceuil et infos de profil

---
+++ src/datas/profil.js
@@ -0,0 +1,50 @@
+
+
+const PROFIL_INTRO = 
+    {
+        photo : "../assets/photo-profil.jpg",
+        nom : "Emmie Gemme",
+        métier : "Développeuse web",
+        apropos : "Après 9 ans d’expérience dans le domaine vétérinaire, j’ai fait une reconversion de carrière vers le développement web. J’ai un profil orienté front end et mets un point d’honneur à produire un code DRY, accessible et pensé pour son prochain. Je poursuis mes études avec un bac +5 afin de me perfectionner et d'ajouter des notions de back end à mon arsenal.",
+        
+    }
+
+
+const EXPE_PRO = [
+    {
+        temps: "",
+        titre: "Développeuse web junior",
+        Description: "Étant une développeuse junior, je suis à la recherche de ma première expérience professionnelle dans ce domaine. Afin de prouver mes capacités et me démarquer, je construis ce site internet en guise de CV."
+
+    }, 
+    {
+        temps: "2024 - Aujourd'hui",
+        titre: "AI Reviewer chez Outlier, Scale AI",
+        Description: "Développement de prompt pour identifier et surmonter les barrières de sécurité et les biais de l'AI. Contribution à l'amélioration des protocoles de sécurité.Étant une développeuse junior, je suis à la recherche de ma première expérience professionnelle dans ce domaine. Afin de prouver mes capacités et me démarquer, je construis ce site internet en guise de CV."
+    }, 
+    {
+        temps: "2019 - 2026",
+        titre: "Technicienne en santé animale au centre DMV",
+        Description: "Rôle d'infirmière en chirurgie spécialisée en centre de référence. Réalisation des préparations chirurgicales, d'anesthésies, d'épidurales, de radiographies, d'analyses de laboratoires, etc. "
+    }
+]
+
+const FORMATION = [
+    {
+        titre: "Certificat professionnel expert en développement logiciel Javascript full-stack",
+        école: "OpenClassrooms, France",
+        temps: "En cours"
+    },
+    {
+        titre: "Certificat professionnel développeeur concepteur logiciel Javascript React",
+        école: "OpenClassrooms, France",
+        temps: "2025 - 2026"
+    },
+    {
+        titre: "Licence professionnelle en santé animale",
+        école: "Collège Lionel-Groulx, Canada",
+        temps: "2014 - 2017"
+    },
+]
+
+export { PROFIL_INTRO, EXPE_PRO, FORMATION }
--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -1,8 +1,16 @@
-
+import { PROFIL_INTRO, EXPE_PRO, FORMATION } from "../../datas/profil"
 
 function Accueil () {
     return (
-        <div className="home">Home</div>
+        <section>
+            <div>
+                <h1>{`${PROFIL_INTRO.nom}`}</h1>
+                <h2>{`${PROFIL_INTRO.métier}`}</h2>
+                <p>{`${PROFIL_INTRO.apropos}`}</p>
+
+            </div>
+            <img sr={`${PROFIL_INTRO.photo}`} alt={`Photo de profil de ${PROFIL_INTRO.nom}`} />
+        </section>
     )
 }
 
