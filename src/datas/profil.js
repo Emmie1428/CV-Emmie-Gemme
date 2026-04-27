8fe58bf7c2acb1f50b6507900753231b21bf365a
Emmie Gemme <mimiemmie@gmail.com>
2026-04-27 08:43:31 -0400

    Feat: Affiche contenu page Accueil sans css

--- src/datas/profil.js
+++ src/datas/profil.js
@@ -2,9 +2,9 @@
 
 const PROFIL_INTRO = 
     {
-        photo : "../assets/photo-profil.jpg",
+        photo : "src/assets/photoprofil.png",
         nom : "Emmie Gemme",
-        métier : "Développeuse web",
+        metier : "Développeuse web",
         apropos : "Après 9 ans d’expérience dans le domaine vétérinaire, j’ai fait une reconversion de carrière vers le développement web. J’ai un profil orienté front end et mets un point d’honneur à produire un code DRY, accessible et pensé pour son prochain. Je poursuis mes études avec un bac +5 afin de me perfectionner et d'ajouter des notions de back end à mon arsenal.",
         
     }
@@ -14,35 +14,35 @@ const EXPE_PRO = [
     {
         temps: "",
         titre: "Développeuse web junior",
-        Description: "Étant une développeuse junior, je suis à la recherche de ma première expérience professionnelle dans ce domaine. Afin de prouver mes capacités et me démarquer, je construis ce site internet en guise de CV."
+        description: "Étant une développeuse junior, je suis à la recherche de ma première expérience professionnelle dans ce domaine. Afin de prouver mes capacités et me démarquer, je construis ce site internet en guise de CV."
 
     }, 
     {
         temps: "2024 - Aujourd'hui",
         titre: "AI Reviewer chez Outlier, Scale AI",
-        Description: "Développement de prompt pour identifier et surmonter les barrières de sécurité et les biais de l'AI. Contribution à l'amélioration des protocoles de sécurité.Étant une développeuse junior, je suis à la recherche de ma première expérience professionnelle dans ce domaine. Afin de prouver mes capacités et me démarquer, je construis ce site internet en guise de CV."
+        description: "Développement de prompt pour identifier et surmonter les barrières de sécurité et les biais de l'AI. Contribution à l'amélioration des protocoles de sécurité.Étant une développeuse junior, je suis à la recherche de ma première expérience professionnelle dans ce domaine. Afin de prouver mes capacités et me démarquer, je construis ce site internet en guise de CV."
     }, 
     {
         temps: "2019 - 2026",
         titre: "Technicienne en santé animale au centre DMV",
-        Description: "Rôle d'infirmière en chirurgie spécialisée en centre de référence. Réalisation des préparations chirurgicales, d'anesthésies, d'épidurales, de radiographies, d'analyses de laboratoires, etc. "
+        description: "Rôle d'infirmière en chirurgie spécialisée en centre de référence. Réalisation des préparations chirurgicales, d'anesthésies, d'épidurales, de radiographies, d'analyses de laboratoires, etc. "
     }
 ]
 
 const FORMATION = [
     {
         titre: "Certificat professionnel expert en développement logiciel Javascript full-stack",
-        école: "OpenClassrooms, France",
+        ecole: "OpenClassrooms, France",
         temps: "En cours"
     },
     {
         titre: "Certificat professionnel développeeur concepteur logiciel Javascript React",
-        école: "OpenClassrooms, France",
+        ecole: "OpenClassrooms, France",
         temps: "2025 - 2026"
     },
     {
         titre: "Licence professionnelle en santé animale",
-        école: "Collège Lionel-Groulx, Canada",
+        ecole: "Collège Lionel-Groulx, Canada",
         temps: "2014 - 2017"
     },
 ]
--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -5,11 +5,28 @@ function Accueil () {
         <section>
             <div>
                 <h1>{`${PROFIL_INTRO.nom}`}</h1>
-                <h2>{`${PROFIL_INTRO.métier}`}</h2>
+                <h2>{`${PROFIL_INTRO.metier}`}</h2>
                 <p>{`${PROFIL_INTRO.apropos}`}</p>
-
+                <img src={`${PROFIL_INTRO.photo}`} alt={`Photo de profil de ${PROFIL_INTRO.nom}`} style={{"width": "150px", "height": "200px"}}/>
             </div>
-            <img sr={`${PROFIL_INTRO.photo}`} alt={`Photo de profil de ${PROFIL_INTRO.nom}`} />
+            <div>
+                {EXPE_PRO.map((expe, index) => (
+                    <div key={index}>
+                        <h3>{`${expe.titre}`}</h3>
+                        <h4>{`${expe.temps}`}</h4>
+                        <p>{`${expe.description}`}</p>
+                    </div>
+                ))}
+            </div>
+            <div>
+                {FORMATION.map((formation, index) => (
+                    <div key={index}>
+                        <h3>{`${formation.titre}`}</h3>
+                        <h4>{`${formation.ecole}`}</h4>
+                        <p>{`${formation.temps}`}</p>
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
